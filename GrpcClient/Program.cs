881d0265106c7078a31225cdd7e6af31edee3e1e
Artem Bayandin <bayandin.artem.official@gmail.com>
2021-03-17 17:47:04 +0300

    some shitty explanation on creating dummy client/server

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+using TestDeps;
 
 namespace GrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        const string Target = "127.0.0.1:50051";
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Channel channel = new Channel(Target, ChannelCredentials.Insecure);
+
+            await channel.ConnectAsync().ContinueWith(task =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("Client connected successfully");
+                }
+            });
+
+            var client = new TestService.TestServiceClient(channel);
+
+            await channel.ShutdownAsync();
+            Console.ReadLine();
         }
     }
 }
--- GrpcServer/Program.cs
+++ GrpcServer/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using Grpc.Core;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace GrpcServer
 {
     class Program
     {
-        static void Main(string[] args)
+        const int Port = 50051;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Server server = null;
+
+            try
+            {
+                server = new Server
+                {
+                    Ports =
+                    {
+                        new ServerPort("localhost", Port, ServerCredentials.Insecure)
+                    }
+                };
+
+                server.Start();
+                Console.WriteLine($"Server is listening on {Port}");
+                Console.ReadLine();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Server failed to start: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Server failed: {ex.Message}");
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    await server.ShutdownAsync();
+                }
+            }
         }
     }
 }
