737c9739ef3ce1e47a15860e629188f0fedeac06
Artem Bayandin <bayandin.artem.official@gmail.com>
2021-03-17 23:42:12 +0300

    minor method refactoring

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -8,37 +8,69 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using TestDeps;
 
 namespace GrpcClient
 {
     class Program
     {
-        const string Target = "127.0.0.1:50051";
+        const string Host = "127.0.0.1";
+        const int Port = 50051;
 
         static async Task Main(string[] args)
         {
-            // ssl
-            var clientCert = await File.ReadAllTextAsync("ssl/client.crt");
-            var clientKey = await File.ReadAllTextAsync("ssl/client.key");
-            var caCrt = await File.ReadAllTextAsync("ssl/ca.crt");
-            var channelCredentials = new SslCredentials(caCrt, new KeyCertificatePair(clientCert, clientKey));
+            var channel = await CreateUnsecureChannel(Host, Port);
 
-            //Channel channel = new Channel(Target, ChannelCredentials.Insecure);
-            Channel channel = new Channel(Target, channelCredentials);
-
-            await channel.ConnectAsync().ContinueWith(task =>
-            {
-                if (task.Status == TaskStatus.RanToCompletion)
-                {
-                    Console.WriteLine("Client connected successfully");
-                }
-            });
-
-            // 1.
-            // var client = new TestService.TestServiceClient(channel);
+            // 1. Test
+            await TestServiceClient(channel);
 
             // 2. Greet unary
-            
+            await GreetUnary(channel);
+
+            // 3. Sum
+            await Sum(channel);
+
+            // 4. Greet server many
+            await GreetServerStreaming(channel);
+
+            // 5. Prime number decomposition
+            await PrimeNumberDecomposition(channel);
+
+            // 6. Client streaming
+            await ClientStreaming(channel);
+
+            // 7. calc average
+            await CalcAverage(channel);
+
+            // 8. bi-di
+            await BiDi(channel);
+
+            // 9. max number
+            await MaxNumber(channel);
+
+            // 10. Errors
+            await ErrorHandling(channel);
+
+            // 11. Deadlines
+            await Deadlines(channel);
+
+            // finish
+            Console.WriteLine("Client is done");
+            await channel.ShutdownAsync();
+            Console.ReadLine();
+        }
+
+        #region Samples
+
+        static async Task TestServiceClient(Channel channel)
+        {
+            WriteHeader("TestServiceClient");
+            var client = new TestService.TestServiceClient(channel);
+        }
+
+        static async Task GreetUnary(Channel channel)
+        {
+            WriteHeader("GreetUnary");
             var client = new GreetingService.GreetingServiceClient(channel);
 
             var greeting = new Greeting
@@ -51,17 +83,20 @@ namespace GrpcClient
 
             var response = await client.GreetAsync(request);
             Console.WriteLine(response.Result);
+        }
 
-            // 3. Sum
-            /*
+        static async Task Sum(Channel channel)
+        {
+            WriteHeader("Sum");
             var client = new CalculatorService.CalculatorServiceClient(channel);
             var sumRequest = new SumRequest { A = 10, B = 3 };
             var response = await client.SumAsync(sumRequest);
             Console.WriteLine($"Response received: {response.Result}\r\n");
-            */
+        }
 
-            // 4. Greet server many
-            /*
+        static async Task GreetServerStreaming(Channel channel)
+        {
+            WriteHeader("GreetServerStreaming");
             var greetManyTimesClient = new GreetingService.GreetingServiceClient(channel);
             var greetManyTimesRequest = new GreetManyTimesRequest
             {
@@ -77,10 +112,11 @@ namespace GrpcClient
             {
                 Console.WriteLine(greetManyTimesResponse.ResponseStream.Current.Result);
             }
-            */
+        }
 
-            // 5. Prime number decomposition
-            /*
+        static async Task PrimeNumberDecomposition(Channel channel)
+        {
+            WriteHeader("PrimeNumberDecomposition");
             var primeNumDecompositionClient = new PrimeNumberDecompositionService.PrimeNumberDecompositionServiceClient(channel);
             var primeNumDecompositionRequest = new DecompositionRequest { Value = 246 };
             var primeNumDecompoResponse = primeNumDecompositionClient.Decompose(primeNumDecompositionRequest);
@@ -88,10 +124,11 @@ namespace GrpcClient
             {
                 Console.WriteLine(primeNumDecompoResponse.ResponseStream.Current.Divider);
             }
-            */
+        }
 
-            // 6. Client streaming
-            /*
+        static async Task ClientStreaming(Channel channel)
+        {
+            WriteHeader("ClientStreaming");
             var clientStreamingClient = new GreetingService.GreetingServiceClient(channel);
             var clientStreamingRequest = new LongGreetRequest { Greeting = "hello" };
             var clientStreamingStream = clientStreamingClient.LongGreet();
@@ -102,10 +139,11 @@ namespace GrpcClient
             await clientStreamingStream.RequestStream.CompleteAsync();
             var clientStreamingResponse = await clientStreamingStream.ResponseAsync;
             Console.WriteLine($"client streaming: {clientStreamingResponse.Result}");
-            */
+        }
 
-            // 7. calc average
-            /*
+        static async Task CalcAverage(Channel channel)
+        {
+            WriteHeader("CalcAverage");
             var avgClient = new CalculatorService.CalculatorServiceClient(channel);
             var avgStream = avgClient.ComputeAverage();
             var rnd = new Random();
@@ -119,10 +157,11 @@ namespace GrpcClient
             await avgStream.RequestStream.CompleteAsync();
             var avgResponse = await avgStream.ResponseAsync;
             Console.WriteLine($"{avgResponse.Result} is an average for {String.Join(", ", avgList)}");
-            */
+        }
 
-            // 8. bi-di
-            /*
+        static async Task BiDi(Channel channel)
+        {
+            WriteHeader("BiDi");
             var bidiClient = new GreetingService.GreetingServiceClient(channel);
             var bidiStream = bidiClient.GreetEveryone();
             var bidiResponseReaderTask = Task.Run(async () =>
@@ -142,10 +181,11 @@ namespace GrpcClient
 
             await bidiStream.RequestStream.CompleteAsync();
             await bidiResponseReaderTask;
-            */
+        }
 
-            // 9. max number
-            /*
+        static async Task MaxNumber(Channel channel)
+        {
+            WriteHeader("MaxNumber");
             var maxClient = new CalculatorService.CalculatorServiceClient(channel);
             var maxStream = maxClient.FindMax();
             var maxStreamReaderTask = Task.Run(async () =>
@@ -165,10 +205,11 @@ namespace GrpcClient
             }
             await maxStream.RequestStream.CompleteAsync();
             await maxStreamReaderTask;
-            */
+        }
 
-            // 10. Errors
-            /*
+        static async Task ErrorHandling(Channel channel)
+        {
+            WriteHeader("ErrorHandling");
             var errorClient = new SqrtService.SqrtServiceClient(channel);
             for (var i = 10; i > -5; i--)
             {
@@ -183,10 +224,11 @@ namespace GrpcClient
                     Console.WriteLine($"Error message: {ex.Message}, detail: {ex.Status.Detail}, status: {ex.Status}");
                 }
             }
-            */
+        }
 
-            // 11. Deadlines
-            /*
+        static async Task Deadlines(Channel channel)
+        {
+            WriteHeader("Deadlines");
             var client = new DeadlineService.DeadlineServiceClient(channel);
             try
             {
@@ -201,11 +243,47 @@ namespace GrpcClient
             {
                 Console.WriteLine($"Err: {e.Status.Detail}");
             }
-            */
-
-            Console.WriteLine("Client is done");
-            await channel.ShutdownAsync();
-            Console.ReadLine();
         }
+
+        #endregion
+
+        #region Channel creation
+
+        static async Task<Channel> CreateSecureChannel(string host, int port)
+        {
+            // ssl
+            var clientCert = await File.ReadAllTextAsync("ssl/client.crt");
+            var clientKey = await File.ReadAllTextAsync("ssl/client.key");
+            var caCrt = await File.ReadAllTextAsync("ssl/ca.crt");
+            var channelCredentials = new SslCredentials(caCrt, new KeyCertificatePair(clientCert, clientKey));
+            return await CreateChannel(host, port, channelCredentials);
+        }
+        static async Task<Channel> CreateUnsecureChannel(string host, int port) => await CreateChannel(host, port, ChannelCredentials.Insecure);
+        static async Task<Channel> CreateChannel(string host, int port, ChannelCredentials credentials)
+        {
+            var channel = new Channel(host, port, credentials);
+
+            await channel.ConnectAsync().ContinueWith(task =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("Client connected successfully");
+                }
+            });
+
+            return channel;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        static int HeaderCounter = 1;
+        static void WriteHeader(string header)
+        {
+            Console.WriteLine($"{Environment.NewLine}{HeaderCounter++} : {header}");
+        }
+
+        #endregion
     }
 }
--- GrpcServer/Program.cs
+++ GrpcServer/Program.cs
@@ -14,15 +14,11 @@ namespace GrpcServer
 {
     class Program
     {
+        const string Host = "127.0.0.1";
         const int Port = 50051;
 
         static async Task Main(string[] args)
         {
-            var serverCert = await File.ReadAllTextAsync("ssl/server.crt");
-            var serverKey = await File.ReadAllTextAsync("ssl/server.key");
-            var caCrt = await File.ReadAllTextAsync("ssl/ca.crt");
-            var credentials = new SslServerCredentials(new List<KeyCertificatePair> { new KeyCertificatePair(serverCert, serverKey) }, caCrt, true);
-
             Server server = null;
 
             try
@@ -34,7 +30,7 @@ namespace GrpcServer
                         GreetingService.BindService(new GreetingServiceImplementation()),
                         CalculatorService.BindService(new CalculatorServiceImplementation()),
                         PrimeNumberDecompositionService.BindService(new PrimeNumberDecompositionServiceImplementation()),
-                        
+
                         // errors
                         SqrtService.BindService(new SqrtServiceImplementation()),
                         // deadlines
@@ -42,8 +38,7 @@ namespace GrpcServer
                     },
                     Ports =
                     {
-                        //new ServerPort("localhost", Port, ServerCredentials.Insecure)
-                        new ServerPort("localhost", Port, credentials)
+                        await CreateUnsecureServerPort(Host, Port)
                     }
                 };
 
@@ -69,5 +64,20 @@ namespace GrpcServer
                 }
             }
         }
+
+        #region ServerPort creation
+
+        static async Task<ServerPort> CreateSecureServerPort(string host, int port)
+        {
+            var serverCert = await File.ReadAllTextAsync("ssl/server.crt");
+            var serverKey = await File.ReadAllTextAsync("ssl/server.key");
+            var caCrt = await File.ReadAllTextAsync("ssl/ca.crt");
+            var credentials = new SslServerCredentials(new List<KeyCertificatePair> { new KeyCertificatePair(serverCert, serverKey) }, caCrt, true);
+            return await CreateServerPort(host, port, credentials);
+        }
+        static async Task<ServerPort> CreateUnsecureServerPort(string host, int port) => await CreateServerPort(host, port, ServerCredentials.Insecure);
+        static async Task<ServerPort> CreateServerPort(string host, int port, ServerCredentials credentials) => await Task.FromResult(new ServerPort(host, port, credentials));
+
+        #endregion
     }
 }
